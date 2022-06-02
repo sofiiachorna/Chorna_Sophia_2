594e9c32dac91930e1c46284dbc54906a95ee604
sofiiachorna <sonya.chorna@gmail.com>
2022-06-02 09:43:32 +0300

    third

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            House house = new House();
+            house.Lock();
+            house.Amount();
+            house.Equals(house);
+            house.ToString();
+            house.GetHashCode();
+
+        }
+    }
+}
